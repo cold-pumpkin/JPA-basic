b6b06eccc108031f741ce158c884ffae646ea07e
adorno <pch4623@gmail.com>
2020-10-11 23:53:09 +0900

    JPA 강좌 프로젝트 파일 모음 디렉토리

---
+++ ex01-hello-jpa/src/test/java/jpabasic/ex01hellojpa/Ex01HelloJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package jpabasic.ex01hellojpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex01HelloJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
