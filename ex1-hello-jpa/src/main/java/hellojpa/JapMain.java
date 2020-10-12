1fb12b042925bf0bcb762e1551441ba12a1ab2b8
adorno <pch4623@gmail.com>
2020-10-12 22:39:11 +0900

    회원 등록 코드 작성

--- ex1-hello-jpa/src/main/java/hellojpa/JapMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JapMain.java
@@ -2,15 +2,32 @@ package hellojpa;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class JapMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 
-        EntityManager entityManager = emf.createEntityManager();
-        entityManager.close();
+        // ** 회원 등록 ** //
+        EntityManager em = emf.createEntityManager(); // 트랜잭션 마다 생성하기
 
+        EntityTransaction tx = em.getTransaction();  // 트랜잭션 받아오기
+        tx.begin();
+
+        try {
+            Member member = new Member();
+            member.setId(2L);
+            member.setName("Chulsoo");
+
+            em.persist(member); // save
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
         emf.close();
     }
 }
