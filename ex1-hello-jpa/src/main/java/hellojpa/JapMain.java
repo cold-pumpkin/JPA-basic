66947304d7144fa7cfd79ec9737b6a8f48648fc0
adorno <pch4623@gmail.com>
2020-10-12 22:51:08 +0900

    페이징 처리 예제 코드

--- ex1-hello-jpa/src/main/java/hellojpa/JapMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JapMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JapMain {
     public static void main(String[] args) {
@@ -17,7 +18,14 @@ public class JapMain {
 
         try {
             Member findMember = em.find(Member.class, 2L);
-            findMember.setName("Youngmi");
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                                    .setFirstResult(1)
+                                    .setMaxResults(10)
+                                    .getResultList();
+            for (Member member : result) {
+                System.out.println("member.name = " + member.getName());
+            }
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
