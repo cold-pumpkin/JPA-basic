2a0853b062c48c44a71ec8c16449d64d653a3276
adorno <pch4623@gmail.com>
2020-10-15 21:01:13 +0900

    엔티티 객체 작성 (데이터 중심 설계 ver)
    현재 테이블의 외래키를 객체의 필드에 넣어둔 데이터 중심 설계 방식으로
    작성
    연관관계 매핑 작업 필요함

---
+++ ex2-jpabook/src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,23 @@
+package jpabook.jpashop;
+
+import javax.persistence.*;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpaapp");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
