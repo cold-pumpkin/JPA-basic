b812014931030b6ad76b4093d427a24535a9c40d
adorno <pch4623@gmail.com>
2020-10-16 23:57:22 +0900

    Team 엔티티 매핑을 위한 외래키 추가: 데이터 중심 설계

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -9,10 +9,14 @@ public class Member {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_seq_generator")
+    @Column(name = "MEMBER_ID")
     private Long id;
 
-    @Column(name = "name")
+    @Column(name = "USER_NAME")
     private String username;
+
+    @Column(name = "TEAM_ID")  // 데이터 중심 설계
+    private Long teamId;
 
     private Integer age;
 
@@ -45,6 +49,14 @@ public class Member {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public Long getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(Long teamId) {
+        this.teamId = teamId;
     }
 
     public Integer getAge() {
