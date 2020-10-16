d2d53c2600f1db7084540348c1698cd1e965212e
adorno <pch4623@gmail.com>
2020-10-17 00:17:19 +0900

    연관관계 매핑 테스트

--- ex1-hello-jpa/src/main/java/hellojpa/JapMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JapMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.swing.plaf.metal.MetalMenuBarUI;
 import java.util.List;
 
 public class JapMain {
@@ -20,6 +21,22 @@ public class JapMain {
             Team team = new Team();
             team.setName("TeamA");
             em.persist(team);
+
+            // 멤버 생성
+            Member member = new Member();
+            member.setUsername("chanho");
+
+            // 팀에 멤버 속하게 하기
+            member.setTeam(team);
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            // 멤버의 팀 조회
+            Member findMember = em.find(Member.class, member.getId());
+            Team findTeam = findMember.getTeam();
+            System.out.println("findTeam name = " + findTeam.getName());
 
             tx.commit();  // flush - commit
         } catch (Exception e) {
