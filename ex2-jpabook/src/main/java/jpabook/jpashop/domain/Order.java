00fb806d413fb88a5e7181100d350b69adb24ddd
adorno <pch4623@gmail.com>
2020-10-18 20:20:41 +0900

    Getter & Setter 추가

--- ex2-jpabook/src/main/java/jpabook/jpashop/domain/Order.java
+++ ex2-jpabook/src/main/java/jpabook/jpashop/domain/Order.java
@@ -23,4 +23,35 @@ public class Order {
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
 }
